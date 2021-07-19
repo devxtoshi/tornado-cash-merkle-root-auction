eaea8f38383bf9e552a769dcd1392aa65c0c1d36
samgos <samuel.jj.gosling@gmail.com>
2021-07-19 21:47:02 +0200

    update TornadoTrees interface

--- contracts/ClaimRewards.sol
+++ contracts/ClaimRewards.sol
@@ -1,3 +1,17 @@
 pragma 0.8.0;
 
-contract ClaimRewards { }
+import "./interfaces/ITornadoTrees.sol";
+
+contract ClaimRewards {
+
+  address public treesProxy = 0x200a79068d8141924b511bc78cb55dca89cf5c2e;
+
+  ITornadoTrees public tornadoTreesV1;
+  ITornadoTrees public tornadoTreesV2;
+
+  constructor() {
+    tornadoTreesV2 = ITornadoTrees(treesProxy);
+    tornadoTreesV1 = ITornadoTrees(tornadoTreesV2.tornadoTreesV1());
+  }
+
+}
--- contracts/interfaces/ITornadoTrees.sol
+++ contracts/interfaces/ITornadoTrees.sol
@@ -1,13 +1,31 @@
 pragma 0.8.0;
 
+struct TreeLeaf {
+   address instance;
+   bytes32 hash;
+   uint256 block;
+ }
+
 interface ITornadoTrees {
 
-  function withdrawalsLength() public view returns (uint8)
+  function updateRoots(TreeLeaf[] calldata _deposits, TreeLeaf[] calldata _withdrawals) external
 
-  function depositsLength() public view returns (uint8)
+  function tornadoTreesV1() external view returns (address)
 
-  function lastProcessedWithdrawalLeaf() public view returns (bytes32)
+  function withdrawalsLength() external view returns (uint256)
 
-  function lastProcessedDepositLeaf() public view returns (bytes32)
+  function depositsLength() external view returns (uint256)
+
+  function lastProcessedWithdrawalLeaf() external view returns (bytes32)
+
+  function lastProcessedDepositLeaf() external view returns (bytes32)
+
+  function withdrawalRoot() external view returns (bytes32)
+
+  function depositRoot() external view returns (bytes32)
+
+  function withdrawals(uint256 i) external view returns (bytes32)
+
+  function deposits(uint256 i) external view returns (bytes32)
 
 }
